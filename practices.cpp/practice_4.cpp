996796ae2bf8c8af9854b98d9e2e3dcedd72248e
soffserdiukk <soffserdiukk@gmail.com>
2026-02-16 15:35:38 +0200

    Update practice_4.cpp

--- practices.cpp/practice_4.cpp
+++ practices.cpp/practice_4.cpp
@@ -3,8 +3,17 @@
 #include <float.h>
 
 // --- ПРОТОТИПИ ФУНКЦІЙ ---
-void task1(); void task2(); void task3(); void task4(); void task5();
-void task6(); void task7(); void task8(); void task9(); void task10(); void task11();
+void task1(); 
+void task2(); 
+void task3(); 
+void task4(); 
+void task5();
+void task6(); 
+void task7(); 
+void task8(); 
+void task9(); 
+void task10(); 
+void task11();
 
 int main() {
     int task_num;
